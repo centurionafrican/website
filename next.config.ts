9aaf882ffdaf44d65bc96ae288fde3cb5954901c
paskab012 <pascalkabikampan@gmail.com>
2025-12-11 23:09:48 +0200

    Fix the build;

--- next.config.ts
+++ next.config.ts
@@ -4,10 +4,6 @@ const nextConfig: NextConfig = {
   images: {
     domains: ["res.cloudinary.com"],
   },
-    i18n: {
-    locales: ["en", "pt", "fr", "es"],
-    defaultLocale: "en",
-  },
 };
 
 export default nextConfig;
